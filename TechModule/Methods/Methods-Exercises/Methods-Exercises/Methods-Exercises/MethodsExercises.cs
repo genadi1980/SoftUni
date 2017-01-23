a1898162b1a1aa0a2ed76185e7a40c17af2969a6
genadi1980 <genadi_georgiev@abv.bg>
2017-01-23 15:39:47 +0200

    Sync Methods-Excersises 01

---
+++ TechModule/Methods/Methods-Exercises/Methods-Exercises/Methods-Exercises/MethodsExercises.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercises
+{
+    class MethodsExercises
+    {
+        static void PrintGreeting(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            PrintGreeting(name);
+        }
+    }
+}
