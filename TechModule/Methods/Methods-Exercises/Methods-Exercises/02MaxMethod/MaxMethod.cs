103fc9aa9f29504aba4be408341d9195a1e8a8ac
genadi1980 <genadi_georgiev@abv.bg>
2017-02-01 21:36:17 +0200

    Sync Methods-Exercises.sln

--- TechModule/Methods/Methods-Exercises/Methods-Exercises/02MaxMethod/MaxMethod.cs
+++ TechModule/Methods/Methods-Exercises/Methods-Exercises/02MaxMethod/MaxMethod.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace _02MaxMethod
 {
-    class Program
+    class MaxMethod
     {
         static void Main(string[] args)
         {
+            var numFirst = int.Parse(Console.ReadLine());
+            var numSecond = int.Parse(Console.ReadLine());
+            var numThird = int.Parse(Console.ReadLine());
+
+            int temp = GetMax(numFirst, numSecond);
+            if (temp > numThird)
+            {
+                Console.WriteLine(temp);
+            }
+            else Console.WriteLine(numThird);
+
+        }
+
+        static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else return b;
         }
     }
 }
