0317899f25f159e1509de9dff8fee6a785256d25
genadi1980 <genadi_georgiev@abv.bg>
2017-04-29 08:13:35 +0300

    Debugging Exam Problems

--- TechModule/Debugging Exam Problems/03CriticalBreakpoint/CriticalBreakpoint.cs
+++ TechModule/Debugging Exam Problems/03CriticalBreakpoint/CriticalBreakpoint.cs
@@ -8,9 +8,55 @@
 
     class CriticalBreakpoint
     {
+        
         static void Main()
         {
+            var input = Console.ReadLine();
 
+            var countLines = 0L;
+
+            List<long> list = new List<long>();
+
+            var sb = new StringBuilder();
+
+            bool isCritical = false;
+
+            while (input != "Break it.")
+            {
+                countLines++;
+
+                string[] line = input.Split(' ');
+
+                long x1 = long.Parse(line[0]);
+                long y1 = long.Parse(line[1]);
+                long x2 = long.Parse(line[2]);
+                long y2 = long.Parse(line[3]);
+
+                sb.Append("Line: [" + x1 + ", " + y1 + ", " + x2 + ", " + y2 + "]" + "\n");
+
+                long result = 0;
+                result = Math.Abs((y2 + x2) - (y1 + x1));
+                list.Add(result);
+
+                input = Console.ReadLine();
+            }
+
+            List<long> list2 = list.Distinct().ToList();
+
+            if (list2.Count > 2)
+            {
+                isCritical = false;
+                Console.WriteLine("Critical breakpoint does not exist.");
+            }
+            else if (list2.Count == 2)
+            {
+                if (list2.Take(0) != list2.Take(1) && list2.Take(0) == 0 || list2.Take(1) == 0)
+                {
+
+                }
+            }
+
+            
         }
     }
 }
--- TechModule/Debugging Exam Problems/04GUnit/GUnit.cs
+++ TechModule/Debugging Exam Problems/04GUnit/GUnit.cs
@@ -4,12 +4,72 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     public class GUnit
     {
         public static void Main()
         {
+            var input = Console.ReadLine();
+
+            Regex regex = new Regex(@"^([A-Z][a-zA-Z0-9]+)\s\|\s([A-Z][a-zA-Z0-9]+)\s\|\s([A-Z][a-zA-Z0-9]+)$");
+
+            Dictionary<string, SortedDictionary<string, SortedSet<string>>> classList = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            while (input != "It's testing time!")
+            {
+                
+                MatchCollection matches = regex.Matches(input);
+
+                foreach (Match match in matches)
+                {
+                    if (match.Length > 2)
+                    {                        
+                        var className = match.Groups[1].ToString();
+                        var methodName = match.Groups[2].ToString();
+                        var unitTestName = match.Groups[3].ToString();
+
+                        if (!classList.ContainsKey(className))
+                        {
+                            classList[className] = new SortedDictionary<string, SortedSet<string>>();
+                        }
+
+                        if (!classList[className].ContainsKey(methodName))
+                        {
+                            classList[className][methodName] = new SortedSet<string>();
+                        }
+
+                        if (!classList[className][methodName].Contains(unitTestName))
+                        {
+                            classList[className][methodName].Add(unitTestName);
+                        }
+                    }
+                }                         
+              
+                input = Console.ReadLine();
+
+               
+            }
+
+            foreach (var units in classList.OrderBy(x => x.Key))
+            {
+                var unit = units.Key;
+                
+                Console.WriteLine($"{unit}:");
+
+                foreach (var methodUnit in units.Value)
+                {
+                    Console.WriteLine($"##{methodUnit.Key}:");
+
+                    foreach (var item in methodUnit.Value)
+                    {
+                        Console.WriteLine($"####{string.Join("", methodUnit.Value)}:");
+                    }
+                }
+                   
+
+            }
         }
     }
 }
