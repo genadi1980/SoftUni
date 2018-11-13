1d8e08d5561d66acef52cff1de17fb48242f5b5b
Genadi Georgiev <genadi_georgiev@abv.bg>
2018-11-13 14:11:48 +0200

    Programming basics

--- C++/C++TaskProject/DebugFile.cpp
+++ C++/C++TaskProject/DebugFile.cpp
@@ -1,43 +1,76 @@
 #include<iostream>
 #include<iomanip>
 #include<cmath>
-#include<map>
+#include<string>
 
 
 using namespace std;
 
 int main() {
+        double balloons = 0.1, flower = 1.5, candles = 0.5, ribbon = 2, spendMoney = 0.0, moneyLeft = 0.0;
+        double budget = 0.0, sum = 0.0, sumBall = 0.0, sumFl = 0.0, sumCan = 0.0, sumRibb=0.0;
+        int number = 0, ball = 0, can = 0, fl = 0, ribb = 0;
+        bool isBudget = false;
 
-    int ageLili = 0, priceToy = 0, counterBro = 0, moneyBD = 0, counter = 0;
-    double priceWash = 0, sumEven = 0, sumOdd = 0, sum = 0;
+        //entrance
+        string command;
+        cin >> budget;
+        cin.ignore();
+        getline(cin, command);
+        cin >> number;
 
-    cin >> ageLili >> priceWash >> priceToy;
 
-    for (int i = 1; i <= ageLili; i++) {
-        if(i%2 == 0){
-            moneyBD +=10;
-            sumEven += moneyBD;
-            counter++;
+        while(command != "stop") {
+                double leftBudget = 0.0;
+                if(command == "balloons"){
+                    ball++;
+                    //sumBall += number * balloons;
+                    sum += number * balloons;
+
+                }
+
+                if(command=="flowers") {
+                    fl++;
+                    sum = number * flower;
+
+                }
+
+                if(command == "ribbon") {
+                    ribb++;
+                    sum = number * ribbon;
+                }
+
+                if (command == "candles") {
+                    can++;
+                    sum = number * candles;
+                }
+
+                budget -= sum;
+                spendMoney = sum;
+                moneyLeft = budget;
+
+                cin.ignore();
+                getline(cin, command);
+                cin >> number;
+
         }
-        else {
-            sumOdd += priceToy;
-        }
 
+        //exit
+        cout.setf(ios::fixed);
+        cout.precision(2);
 
-
-    }
-    sum = (sumEven + sumOdd - counter);
-
-
-        if(sum > priceWash){
-
-           cout << fixed << setprecision(2) << "Yes! " << sum - priceWash << endl;
+        if(isBudget) {
+            cout << "All money is spent!" << endl;
         } else {
-
-        cout << fixed << setprecision(2) << "No! " << priceWash - sum << endl;
+            cout << "Spend money: " << spendMoney << endl;
+            cout << "Money left: "  << moneyLeft << endl;
         }
-    return 0;
-}
 
+        cout << "Purchased decoration is " << ball <<" balloons, " << ribb << " m ribbon, "
+                    << fl << " flowers and " << can <<" candles." << endl;
+
+
+        return 0;
+}
 
 
